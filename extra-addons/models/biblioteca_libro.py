bd1d2e3457c729f28c3bbb3422cfe59991bb8205
stormk203-ui <stormk203@gmail.com>
2025-12-12 20:19:25 +0100

    Add BibliotecaLibro model with fields for title, author, and date

--- extra-addons/models/biblioteca_libro.py
+++ extra-addons/models/biblioteca_libro.py
@@ -1,1 +1,9 @@
+from odoo import models, fields
 
+class BibliotecaLibro(models.Model):
+    _name = 'biblioteca.libro'
+    _description = 'Libro de la biblioteca'
+
+    name = fields.Char(string='Título', required=True)
+    autor = fields.Char(string='Autor', default='Marcos')
+    fecha = fields.Date(string='Fecha de publicación')
