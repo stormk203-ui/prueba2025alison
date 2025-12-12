d0847e54e6f83891721717e5b92a16420d66df4a
stormk203-ui <stormk203@gmail.com>
2025-12-12 20:25:01 +0100

    Change default author from 'Marcos' to 'Sergio'

--- extra-addons/models/biblioteca_libro.py
+++ extra-addons/models/biblioteca_libro.py
@@ -3,7 +3,6 @@ from odoo import models, fields
 class BibliotecaLibro(models.Model):
     _name = 'biblioteca.libro'
     _description = 'Libro de la biblioteca'
-
     name = fields.Char(string='Título', required=True)
-    autor = fields.Char(string='Autor', default='Marcos')
+    autor = fields.Char(string='Autor', default='Sergio')
     fecha = fields.Date(string='Fecha de publicación')
